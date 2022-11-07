942d009639a708f3a61ee1291fd587702219e77c
Davit <daa@codemotion.us>
2022-11-07 12:47:49 +0400

    feat: change footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -8,59 +8,54 @@ import {
 } from './Footer.styled';
 
 const Footer = ({ isDark }) => {
-  return (
-    <StyledFooterWrapper>
-      <StyledFooterLinks>
-        <StyledFooterLink>
-          <Link href="/first">Privacy policy</Link>
-        </StyledFooterLink>
-        <StyledFooterLink>
-          <Link href="/second">User agreement</Link>
-        </StyledFooterLink>
-        <StyledFooterLink>
-          <Link href="/third">Terms of use</Link>
-        </StyledFooterLink>
-      </StyledFooterLinks>
-      <StyledFooterLinks>
-        <StyledFooterLink>
-          <a
-            href="https://twitter.com/derby_finance"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Twitter isDark={isDark} />
-          </a>
-        </StyledFooterLink>
-        <StyledFooterLink>
-          <a
-            href="https://discord.gg/DyxRxs9mQ6"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Discord isDark={isDark} />
-          </a>
-        </StyledFooterLink>
-        <StyledFooterLink>
-          <a
-            href="https://derby-finance.gitbook.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Github isDark={isDark} />
-          </a>
-        </StyledFooterLink>
-        <StyledFooterLink>
-          <a
-            href="https://medium.com/derbyfinance"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <MediumFooter isDark={isDark} />
-          </a>
-        </StyledFooterLink>
-      </StyledFooterLinks>
-    </StyledFooterWrapper>
-  );
+  const socialLinks = [
+    {
+      name: 'Twitter',
+      icon: <Twitter />,
+      link: 'https://twitter.com/derby_finance',
+    },
+    {
+      name: 'Discord',
+      icon: <Discord />,
+      link: 'https://discord.gg/DyxRxs9mQ6',
+    },
+    {
+      name: 'Github',
+      icon: <Github />,
+      link: 'https://derby-finance.gitbook.io/',
+    },
+    {
+      name: 'Medium',
+      icon: <MediumFooter />,
+      link: 'https://medium.com/derbyfinance',
+    },
+  ];
+  {
+    return (
+      <StyledFooterWrapper>
+        <StyledFooterLinks>
+          <StyledFooterLink>
+            <Link href="/first">Privacy policy</Link>
+          </StyledFooterLink>
+          <StyledFooterLink>
+            <Link href="/second">User agreement</Link>
+          </StyledFooterLink>
+          <StyledFooterLink>
+            <Link href="/third">Terms of use</Link>
+          </StyledFooterLink>
+        </StyledFooterLinks>
+        <StyledFooterLinks>
+          {socialLinks.map(({ icon, link, name }) => (
+            <StyledFooterLink key={name}>
+              <a href={link} target="_blank" rel="noreferrer">
+                {icon}
+              </a>
+            </StyledFooterLink>
+          ))}
+        </StyledFooterLinks>
+      </StyledFooterWrapper>
+    );
+  }
 };
 
 export default Footer;

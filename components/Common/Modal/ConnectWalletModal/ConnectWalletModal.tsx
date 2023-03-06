fed2dc6582e6c8f1a2dc1e7ba872ad51832a5694
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-03-06 14:10:08 +0200

    Updated walletDetected

--- components/Common/Modal/ConnectWalletModal/ConnectWalletModal.tsx
+++ components/Common/Modal/ConnectWalletModal/ConnectWalletModal.tsx
@@ -2,15 +2,15 @@ import React, { FC, useState } from 'react';
 import ErrorMessage from '@components/Common/ErrorMessage/ErrorMessage';
 import { StyledConnectDisclaimer } from '@components/Layout/NavBar/NavBar.styled';
 import SignIn from '@components/SignIn/SignIn';
+import close from '@icons/close.svg';
 import { DFConnectIcon } from '@icons/index';
+import Image from 'next/image';
 import Modal from '../Modal';
-import close from '@icons/close.svg'
 import {
+  StyledCloseIcon,
   StyledModalConnectOptions,
   StyledModalLogoAndText,
-  StyledCloseIcon
 } from '../Modal.styled';
-import Image from 'next/image';
 
 type ConnectWalletModalProps = {
   isOpen: boolean;
@@ -25,23 +25,23 @@ const ConnectWalletModal: FC<ConnectWalletModalProps> = ({
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <StyledCloseIcon>
-      <Image onClick={()=> onClose()} src={close} alt={"close"}/>
+        <Image onClick={() => onClose()} src={close} alt={'close'} />
       </StyledCloseIcon>
-        <StyledModalLogoAndText>
-          <DFConnectIcon />
-          <h3>Connect Wallet</h3>
-          <h4>to start using Derby Finance</h4>
-        </StyledModalLogoAndText>
-        {walletDetected && (
-          <ErrorMessage message="wallet not detected. Connect or install wallet and retry" />
-        )}
-        <StyledModalConnectOptions>
-          <SignIn closeModal={onClose} />
-        </StyledModalConnectOptions>
-        <StyledConnectDisclaimer>
-          By connecting, I accept Derby Finance’s
-          <a href="#">terms of Service </a>
-        </StyledConnectDisclaimer>
+      <StyledModalLogoAndText>
+        <DFConnectIcon />
+        <h3>Connect Wallet</h3>
+        <h4>to start using Derby Finance</h4>
+      </StyledModalLogoAndText>
+      {!walletDetected && (
+        <ErrorMessage message="wallet not detected. Connect or install wallet and retry" />
+      )}
+      <StyledModalConnectOptions>
+        <SignIn closeModal={onClose} />
+      </StyledModalConnectOptions>
+      <StyledConnectDisclaimer>
+        By connecting, I accept Derby Finance’s
+        <a href="#">terms of Service </a>
+      </StyledConnectDisclaimer>
     </Modal>
   );
 };

b51f0928cd1cf24990b97a091f6a12397162f536
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-03-23 12:08:29 +0200

    Update nonConnected wallet styles

--- components/pageComponents/VaultsPage/WalletInformation/WalletInfo.styled.js
+++ components/pageComponents/VaultsPage/WalletInformation/WalletInfo.styled.js
@@ -11,8 +11,8 @@ export const StyledNotConnectedWrapper = styled.div`
   text-align: center;
   color: ${({ theme }) => theme.colors.textSecondary};
   background: ${({ theme }) => theme.colors.gradientWallet};
-  padding: 148px 4vh 133px;
-  border-radius: 28px;
+  padding: 148px 60px 133px;
+  border-radius: 6px;
   border: ${({ theme }) => theme.colors.borderMain} 1px solid;
   height: 550px;
   width: 425px;

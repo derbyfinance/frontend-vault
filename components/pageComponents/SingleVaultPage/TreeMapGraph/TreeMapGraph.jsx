ecc12326d05d94435679f5e02ebd1c0f7eb18007
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-03-29 09:29:51 +0300

    Updated TreeMapGraph

--- components/pageComponents/SingleVaultPage/TreeMapGraph/TreeMapGraph.jsx
+++ components/pageComponents/SingleVaultPage/TreeMapGraph/TreeMapGraph.jsx
@@ -1,4 +1,4 @@
-import ethereum from '@icons/EthereumNetwork';
+import { useEffect, useState } from 'react';
 import {
   BarElement,
   CategoryScale,
@@ -11,6 +11,7 @@ import {
 import { color } from 'chart.js/helpers';
 import { TreemapController, TreemapElement } from 'chartjs-chart-treemap';
 import { Chart } from 'react-chartjs-2';
+import { ApiService } from 'services/api.service';
 import styled from 'styled-components';
 
 ChartJS.register(
@@ -26,75 +27,101 @@ ChartJS.register(
 ChartJS.defaults.font.family = 'Roboto';
 ChartJS.defaults.font.size = 18;
 
-const data = [
-  { name: 'ETH', icon: ethereum, dataCoverage: 0.314, colorProps: 1 },
-  { name: 'BSC', icon: ethereum, dataCoverage: 0.208, colorProps: 0.9 },
-  { name: 'OPT', icon: ethereum, dataCoverage: 0.102, colorProps: 0.8 },
-  { name: 'AVA', icon: ethereum, dataCoverage: 0.247, colorProps: 0.7 },
-  { name: 'POL', icon: ethereum, dataCoverage: 0.081, colorProps: 0.6 },
-  { name: 'ARB', icon: ethereum, dataCoverage: 0.078, colorProps: 0.5 },
-  { name: 'FAN', icon: ethereum, dataCoverage: 0.032, colorProps: 0.4 },
-];
-const options = {
-  plugins: {
-    legend: {
-      display: false,
-    },
-    tooltip: {
-      displayColors: false,
-      callbacks: {
-        title(items) {
-          return items[0].raw._data.name;
-        },
-        label(item) {
-          const {
-            _data: { dataCoverage },
-          } = item.raw;
-          return [`Data Coverage: ${dataCoverage * 100}%`];
-        },
-      },
-    },
-  },
-};
-
-const config = {
-  type: 'treemap',
-  data: {
-    datasets: [
-      {
-        tree: data,
-        key: 'dataCoverage',
-        labels: {
-          display: true,
-          formatter: (context) => [
-            context.raw._data.name,
-            context.raw._data.dataCoverage * 100 + '%',
-          ],
-          color: '#fff',
-          font: {
-            weight: 500,
-          },
-        },
-        backgroundColor(context) {
-          if (context.type !== 'data') return 'transparent';
-          const { dataCoverage, colorProps } = context.raw._data;
-          return dataCoverage === 0
-            ? color('grey').rgbString()
-            : color('#39079B').alpha(colorProps).rgbString();
-        },
-      },
-    ],
-  },
-};
-
 const StyledTreeMap = styled.div`
   margin-bottom: 84px;
-  @media only screen and (max-width: 1130px) {
-    width:700px;
-  }
 `;
 
 export default function TreemapChart() {
+  const [chainAllocations, setChainAllocations] = useState([]);
+
+  useEffect(() => {
+    getVaultDataById();
+  }, []);
+
+  const getVaultDataById = async () => {
+    try {
+      const { data } = await ApiService.getUserVaultById('');
+      let newChainAllocations = data.data.chainAllocations.map((el, index) => {
+        return {
+          name: Object.keys(el)[0],
+          dataCoverage: Object.values(el)[0] / 100,
+          colorProps: 1 - Number(`0.${index}`),
+        };
+      });
+      setChainAllocations(newChainAllocations);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const options = {
+    plugins: false,
+    //TODO
+    // legend: {
+    //   display: false,
+    // },
+    // tooltip: {
+    //   displayColors: false,
+    //   callbacks: {
+    //     title(items) {
+    //       return items[0].raw._data.name;
+    //     },
+    //     label(item) {
+    //       const {
+    //         _data: { dataCoverage },
+    //       } = item.raw;
+    //       return [`Data Coverage: ${dataCoverage * 100}%`];
+    //     },
+    //   },
+    // },
+    // },
+  };
+
+  function colorFromRaw(ctx) {
+    if (ctx.type !== 'data') return 'transparent';
+    const { dataCoverage, colorProps } = ctx.raw._data;
+    return dataCoverage === 0
+      ? color('grey').rgbString()
+      : color('#39079B')
+          .alpha(colorProps + 0.1)
+          .rgbString();
+  }
+
+  const config = {
+    type: 'treemap',
+    data: {
+      datasets: [
+        {
+          tree: chainAllocations,
+          key: 'dataCoverage',
+          hoverBackgroundColor: (ctx) => colorFromRaw(ctx),
+          labels: {
+            display: true,
+            hoverFont: {
+              size: 20,
+            },
+            formatter: (context) => {
+              return [
+                context.raw._data.name,
+                context.raw._data.dataCoverage * 100 + '%',
+              ];
+            },
+            color: '#fff',
+            font: {
+              weight: 500,
+            },
+          },
+          backgroundColor(context) {
+            if (context.type !== 'data') return 'transparent';
+            const { dataCoverage, colorProps } = context.raw._data;
+            return dataCoverage === 0
+              ? color('grey').rgbString()
+              : color('#39079B').alpha(colorProps).rgbString();
+          },
+        },
+      ],
+    },
+  };
   return (
     <StyledTreeMap>
       <Chart type="treemap" data={config.data} options={options} />

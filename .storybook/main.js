553da442f54448d43b8f3d723b87f67c61859f3e
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-03-30 15:58:56 +0300

    Create story book with header & footer

---
+++ .storybook/main.js
@@ -0,0 +1,33 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+module.exports = {
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
+
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling',
+  ],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
+  framework: '@storybook/react',
+};
---
+++ stories/NavBar.styled.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const StyledNavBarWrapper = styled.nav`
+  width: 100%;
+  top: 0;
+  background: ${({ theme }) => theme.colors.backgroundMain};
+  padding: 64px 30px 40px;
+  z-index: 20;
+  font-size: 15px;
+`;
+
+export const StyledNavBarContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+export const StyledNavLinks = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+`;
+
+export const StyledNavLink = styled.li`
+  font-family: ${({ active }) => (active ? 'Slab-Medium' : 'Slab-Light')};
+  font-size: 16px;
+  & a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.textMain};
+    padding: 10px 12px;
+    border-bottom: ${({ active, theme }) =>
+      active ? `${theme.colors.borderTertiary} solid 3px` : 'none'};
+    @media only screen and (max-width: 860px) {
+      padding: 10px 6px;
+    }
+  }
+`;
+
+export const StyledConnectDisclaimer = styled.div`
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.textPrimary};
+  margin-top: 54px;
+  & a {
+    background: ${({ theme }) => theme.colors.gradientTwo};
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    margin-left: 4px;
+  }
+`;

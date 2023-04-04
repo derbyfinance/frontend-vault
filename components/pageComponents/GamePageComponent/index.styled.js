1c0fde352587feacfcffe129c5c4bf2d32ee21a2
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-04-04 10:09:23 +0300

    Created race information

--- components/pageComponents/GamePageComponent/index.styled.js
+++ components/pageComponents/GamePageComponent/index.styled.js
@@ -10,3 +10,17 @@ export const GamePageWrapper = styled.div`
   width: 100%;
 `;
 
+export const GamePageContainer = styled.div`
+  padding: 30px 0 0 0;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 16px;
+  border-top: 1px solid #e2e2e2;
+  position: relative;
+  width: 100%;
+`;
+
+export const GameDescriptionContainer = styled.div`
+  width: 800px;
+`;
--- components/pageComponents/GamePageComponent/index.tsx
+++ components/pageComponents/GamePageComponent/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { FC } from 'react';
+import SingleVaultDescription from '../SingleVaultPage/SingleVaultDescription/SingleVaultDescription';
 import RaceStatisticsComponent from './RaceStatistics/RaceStatisticsComponent';
-import { GamePageWrapper } from './index.styled';
+import {
+  GameDescriptionContainer,
+  GamePageContainer,
+  GamePageWrapper,
+} from './index.styled';
 
-const GamePageComponent = () => {
+const description: string =
+  "Oh no, don't touch that. That's some new specialized weather sensing equipment. Hey, hey, I've seen this one, I've seen this one. This is a classic, this is where Ralph dresses up as the man from space. Something wrong with the starter, so I hid it. Just turn around, McFly, and walk away. Are you deaf, McFly? Close the door and beat it. Well, aren't you going up to the lake tonight, you've been planning it for two weeks.";
+
+const GamePageComponent: FC = () => {
   return (
     <GamePageWrapper>
-      <RaceStatisticsComponent />
+      <GamePageContainer>
+        <GameDescriptionContainer>
+          <SingleVaultDescription
+            description={description}
+            vault={'Welcome to The Race'}
+            imagePath={undefined}
+          />
+        </GameDescriptionContainer>
+        <RaceStatisticsComponent />
+      </GamePageContainer>
     </GamePageWrapper>
   );
 };

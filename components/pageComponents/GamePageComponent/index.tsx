047d6c0250d5a52f6a5592040cfa76c98a6978c7
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-04-05 14:15:30 +0300

    Adjust name

--- components/pageComponents/GamePageComponent/index.tsx
+++ components/pageComponents/GamePageComponent/index.tsx
@@ -19,7 +19,7 @@ import {
 const description: string =
   "Oh no, don't touch that. That's some new specialized weather sensing equipment. Hey, hey, I've seen this one, I've seen this one. This is a classic, this is where Ralph dresses up as the man from space. Something wrong with the starter, so I hid it. Just turn around, McFly, and walk away. Are you deaf, McFly? Close the door and beat it. Well, aren't you going up to the lake tonight, you've been planning it for two weeks.";
 
-const headers: string[] = ['NAME', 'MEDALS', 'STAKED', 'PErformance'];
+const headers: string[] = ['NAME', 'MEDALS', 'STAKED', 'PERFORMANCE'];
 
 const GamePageComponent: FC = () => {
   const [raceData, setRaceData] = useState<IRace>();

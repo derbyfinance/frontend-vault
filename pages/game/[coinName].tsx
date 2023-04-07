2172892fe11ac59266adca525c0c3f0bd271be45
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-04-07 11:54:34 +0300

    Created race information

---
+++ pages/game/[coinName].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@components/Layout/Layout';
+import SingleGamePage from '@components/pageComponents/SingleGamePage/SingleGamePage';
+
+const SingleVaultPage = () => {
+  return (
+    <Layout>
+      <SingleGamePage />
+    </Layout>
+  );
+};
+
+export default SingleVaultPage;

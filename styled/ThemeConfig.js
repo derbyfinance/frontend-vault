59355e63ed99c1fcb10e5429ef4364e093be5e49
PavloOliinykEA <pavlo.oliinyk@codegenerationsoftware.com>
2023-04-21 11:54:29 +0300

    Upated global styles

--- styled/ThemeConfig.js
+++ styled/ThemeConfig.js
@@ -1,53 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-
-@font-face {
-	font-family: "Roboto-Bold";
-	src: url("/fonts/roboto/Roboto-Bold.ttf");
-	font-weight: 700;
-}
-
-@font-face {
-	font-family: "Roboto-Medium";
-	src: url("/fonts/roboto/Roboto-Medium.ttf");
-	font-weight: 500;
-}
-
-@font-face {
-	font-family: "Roboto-Regular";
-	src: url("/fonts/roboto/Roboto-Regular.ttf");
-	font-weight: 400;
-}
-
-@font-face {
-	font-family: "Roboto-Light";
-	src: url("/fonts/roboto/Roboto-Light.ttf");
-	font-weight: 300;
-}
-
-
-
-@font-face {
-	font-family: "Slab-Medium";
-	src: url("/fonts/roboto-slab/RobotoSlab-Medium.ttf");
-	font-weight: 500;
-}
-
-@font-face {
-	font-family: "Slab-Regular";
-	src: url("/fonts/roboto-slab/RobotoSlab-Regular.ttf");
-	font-weight: 400;
-}
-
-@font-face {
-	font-family: "Slab-Light";
-	src: url("/fonts/roboto-slab/RobotoSlab-Light.ttf");
-	font-weight: 300;
-}
-
-
-
 *,
 *::before,
 *::after {
